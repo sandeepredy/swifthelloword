7c71b0a7d690e3c090c085a7b94e118e6d3e7923
sandeepredy <42674037+sandeepredy@users.noreply.github.com>
2024-05-09 12:02:21 -0500

    Samples Xcode

---
+++ swiftuihelloworld/swiftuihelloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuihelloworldApp.swift
+//  swiftuihelloworld
+//
+//  Created by Chada, Sandeep Reddy (S.) on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftuihelloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
